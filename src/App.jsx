404dfb4fcd8faa3abebf56354758254b1e146d31
’Marina <148355147+MaRos10@users.noreply.github.com>
2024-07-27 09:42:38 +0200

    Changed structure to header

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,20 @@ import "./index.css";
 function App() {
   return (
     <div className="app">
-      <header className="header">
-        <nav>
-          <Link to="info" smooth={true} duration={500}>
-            Info
-          </Link>
-          <Link to="calendar" smooth={true} duration={500}>
-            Kalender
-          </Link>
-          <Link to="contact" smooth={true} duration={500}>
-            Kontakt
-          </Link>
-        </nav>
-      </header>
-
       <Element name="info" className="section section-main">
+        <header className="header">
+          <nav>
+            <Link to="info" smooth={true} duration={500}>
+              HEM
+            </Link>
+            <Link to="calendar" smooth={true} duration={500}>
+              KALENDER
+            </Link>
+            <Link to="contact" smooth={true} duration={500}>
+              KONTAKT
+            </Link>
+          </nav>
+        </header>
         <h2 className="main-h2">HOVSHAGA IF</h2>
       </Element>
 
