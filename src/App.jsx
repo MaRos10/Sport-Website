5c33db51dba2f374e748b7f500a7ac8119a2a290
’Marina <148355147+MaRos10@users.noreply.github.com>
2024-08-04 15:43:36 +0200

    Flexbox layout for calendar and carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Element } from "react-scroll";
+import "./index.css";
 import Calendar from "./components/calendar/Calendar";
 import HeroSection from "./components/hero-section/HeroSection";
 import MapSection from "./components/map-section/MapSection";
 import Footer from "./components/footer/Footer";
-import "./index.css";
 import CustomCarousel from "./components/carousel/Carousel";
 
 export default function App() {
@@ -12,8 +12,14 @@ export default function App() {
       <HeroSection />
       <Element name="calendar" className="section">
         <h2 className="section-h2">Matchkalender</h2>
-        <Calendar />
-        <CustomCarousel />
+        <div className="calendar-container">
+          <div className="calendar">
+            <Calendar />
+          </div>
+          <div className="carousel">
+            <CustomCarousel />
+          </div>
+        </div>
       </Element>
       <MapSection />
       <Footer />
